e138d42611e1a6126a2dfd689e5ea3280ff1b924
M1sTaker <69234606+M1sTaker@users.noreply.github.com>
2023-03-13 17:11:22 +0800

    修改导航中的错误

--- navigate.py
+++ navigate.py
@@ -1,30 +1,33 @@
 import numpy as np
+import math
 
 def move_to_xy(robot, des_x, des_y):
-    robot_face_angle = robot['face_angle']
+    robot_face_angle = robot['face_angle']#机器人朝向
 
-    robot_xy = np.array(robot['x'], robot['y'])#机器人坐标
-    des_xy = np.array(des_x, des_y)#终点坐标
+    robot_xy = np.array([robot['x'], robot['y']])#机器人坐标
+    des_xy = np.array([des_x, des_y])#终点坐标
 
-    face_vec = np.array(robot['line_speed_x'], robot['line_speed_y']) # 机器人面向向量
     xy_robot_vec = des_xy - robot_xy# xy-机器人坐标向量
+    xy_robot_vec_angle = math.atan2(xy_robot_vec[1] , xy_robot_vec[0])# xy-机器人向量朝向
 
     distance = np.linalg.norm(robot_xy - des_xy)#计算距离
 
-    if(np.dot(face_vec, xy_robot_vec.T) >= 0):# 机器人面向向量与(工作台-机器人)向量小于90°
-        if(robot_face_angle == np.arctan(xy_robot_vec[1] / xy_robot_vec[0])):# 夹角为0不改变方向
-            return 9999, 0
-        line_speed = 9999
-        if(robot_face_angle > np.arctan(xy_robot_vec[1] / xy_robot_vec[0])):# 机器人向右转(顺时针)
-            angle_speed = -np.pi
+
+    # 机器人面向向量与(工作台-机器人)向量小于90°
+    if (np.abs(robot_face_angle - xy_robot_vec_angle) <= np.pi/2):
+        if(robot_face_angle == xy_robot_vec_angle):# 夹角为0不改变方向
+            return 9999.0, 0
+        line_speed = 9999.0
+        if(robot_face_angle > xy_robot_vec_angle):# 机器人向右转(顺时针)
+            angle_speed = -3.0
         else:
-            angle_speed = np.pi
+            angle_speed = 3.0
     else:# 机器人面向向量与(工作台-机器人)向量大于90°
-        line_speed = 0.1
-        if (robot_face_angle > np.arctan(xy_robot_vec[1] / xy_robot_vec[0])):  # 机器人向右转(顺时针)
-            angle_speed = -np.pi
+        line_speed = 9999
+        if (robot_face_angle > xy_robot_vec_angle):  # 机器人向右转(顺时针)
+            angle_speed = -3.0
         else:
-            angle_speed = np.pi
+            angle_speed = 3.0
 
     if(distance < 0.8): #快到终点时减速
         line_speed = 0.1
