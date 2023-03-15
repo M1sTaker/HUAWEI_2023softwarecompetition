3c771a1a05e993e53c35edecfce5b17a186e6196
M1sTaker <69234606+M1sTaker@users.noreply.github.com>
2023-03-15 11:46:26 +0800

    修改avoiCrash，增加防撞墙功能

--- navigate.py
+++ navigate.py
@@ -16,20 +16,23 @@ def move_to_xy(robot, des_x, des_y):
     # 机器人面向向量与(工作台-机器人)向量小于90°
     if (np.abs(robot_face_angle - xy_robot_vec_angle) <= np.pi/2):
         if(robot_face_angle == xy_robot_vec_angle):# 夹角为0不改变方向
-            return 9999.0, 0
-        line_speed = 9999.0
+            return 3.0, 0
+        line_speed = 3.0
         if(robot_face_angle > xy_robot_vec_angle):# 机器人向右转(顺时针)
-            angle_speed = -3.0
+            angle_speed = -2.0
         else:
             angle_speed = 3.0
     else:# 机器人面向向量与(工作台-机器人)向量大于90°
-        line_speed = 9999
+        line_speed = 3.0
         if (robot_face_angle > xy_robot_vec_angle):  # 机器人向右转(顺时针)
-            angle_speed = -3.0
+            angle_speed = -2.0
         else:
-            angle_speed = 3.0
+            angle_speed = 2.0
 
-    if(distance < 0.8): #快到终点时减速
+    if(distance < 0.5): #快到终点时减速
         line_speed = 0.1
+        if(distance < 0.2): #距离小于一定值时停止
+            line_speed = 0
+            angle_speed = 0
 
     return line_speed, angle_speed
