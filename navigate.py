282024ceaee90016c1576839c5b874cea99c78ab
M1sTaker <69234606+M1sTaker@users.noreply.github.com>
2023-03-15 17:20:01 +0800

    优化了navigate

--- navigate.py
+++ navigate.py
@@ -1,8 +1,9 @@
 import numpy as np
 import math
 
-def outer(arr1, arr2):#计算外积
+def outer(arr1, arr2):#计算外积,用于判断转向的方向
     return arr1[0] * arr2[1] - arr1[1] * arr2[0]
+
 def move_to_xy(robot, des_x, des_y):
     robot_face_angle = robot['face_angle']#机器人朝向
     robot_face_vec = np.array([np.cos(robot_face_angle), np.sin(robot_face_angle)])#机器人朝向向量
@@ -16,23 +17,22 @@ def move_to_xy(robot, des_x, des_y):
     distance = np.linalg.norm(robot_xy - des_xy)#计算距离
 
     #朝向：弧度[-pi，pi]  速度：m/s [-2,6]  旋转速度：弧度/s [-pi,pi]
-    # 机器人面向向量与(工作台-机器人)向量小于90°
-    # if (np.abs(robot_face_angle) + np.abs(xy_robot_vec_angle) <= np.pi/2 or np.abs(robot_face_angle) + np.abs(xy_robot_vec_angle) >= 2*np.pi - np.pi/2):
-    if(robot_face_angle == xy_robot_vec_angle):# 夹角为0不改变方向
-        return 6.0, 0
-    line_speed = 4.0
-    if(outer(robot_face_vec, xy_robot_vec) <= 0):# 机器人向右转(顺时针)
-        angle_speed = -3.0
-    else:# 机器人向左转(逆时针)
-         angle_speed = 3.0
-    # 机器人面向向量与(工作台-机器人)向量大于90°
-    #     line_speed = 3.0
-    #     if (outer(robot_face_vec, xy_robot_vec) <= 0):  # 机器人向右转(顺时针)
-    #         angle_speed = -3.0
-    #     else:
-    #         angle_speed = 3.0
+    if(np.dot(robot_face_vec, xy_robot_vec) >= 0):# 机器人面向向量与(工作台-机器人)向量小于90°
+        if(robot_face_angle == xy_robot_vec_angle):# 夹角为0不改变方向
+            return 6.0, 0
+        line_speed = 6.0
+        if(outer(robot_face_vec, xy_robot_vec) <= 0):# 机器人向右转(顺时针)
+            angle_speed = -3.0
+        else:# 机器人向左转(逆时针)
+             angle_speed = 3.0
+    else:
+        line_speed = 2.0
+        if (outer(robot_face_vec, xy_robot_vec) <= 0):  # 机器人向右转(顺时针)
+            angle_speed = -9.0
+        else:  # 机器人向左转(逆时针)
+            angle_speed = 9.0
 
-    if(distance < 0.5): #快到终点时减速
+    if(distance < 1.5): #快到终点时减速
         line_speed = 2.0
         if(distance < 0.2): #距离小于一定值时停止
             line_speed = 1.0
